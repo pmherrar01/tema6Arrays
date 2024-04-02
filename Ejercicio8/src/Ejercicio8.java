c56353c32ed7245624e28b956bb332cafcb7b023
pmherrar01 <pmherrerar01@educarex.es>
2024-04-02 20:26:02 +0200

    Ejercicio8 por terminar

---
+++ Ejercicio8/src/Ejercicio8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio8 {
+	
+	public static int pedirNumero() {
+		Scanner entrada = new Scanner(System.in);
+		int num;
+		System.out.println("introduce um numero de 5 cifras:");
+		num = entrada.nextInt();
+		return num;
+	}
+
+	public static void rellenarVector() {
+		
+	}
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
