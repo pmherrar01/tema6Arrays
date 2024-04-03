f583f91b079bba996773f3c54cfbf8a37da1689f
pmherrar01 <pmherrerar01@educarex.es>
2024-04-03 17:35:07 +0200

    Ejercicio8 terminado

--- Ejercicio8/src/Ejercicio8.java
+++ Ejercicio8/src/Ejercicio8.java
@@ -10,13 +10,27 @@ public class Ejercicio8 {
 		return num;
 	}
 
-	public static void rellenarVector() {
-		
-	}
+    public static void rellenarVector(int vNum[], int num){
+        int i, resto;
+        for(i = 0;i < vNum.length;i++){
+            resto = num % 10;
+            vNum[i] = resto; 
+            num = num / 10;
+        }
+    }
+    
+    public static void mostrarVector(int vNum[]){
+        int i;
+        for(i = 0;i < vNum.length;i++){
+            System.out.print(vNum[i]);
+        }
+    }
 	
 	public static void main(String[] args) {
-	
-
+        int num = pedirNumero();
+        int[] vNum = new int [5];
+        rellenarVector(vNum, num);
+        mostrarVector(vNum);
 	}
 
 }
