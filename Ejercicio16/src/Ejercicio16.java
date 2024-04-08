1f64ea11bcddc1123c503c3697e8f269dc717c2e
pmherrar01 <pmherrerar01@educarex.es>
2024-04-08 18:57:45 +0200

    Ejercicio 16 por terminar

--- Ejercicio16/src/Ejercicio16.java
+++ Ejercicio16/src/Ejercicio16.java
@@ -2,108 +2,124 @@ import java. util.Scanner;
 
 public class Ejercicio16 {
 
-	final static int ALUM = 6;
-	final static int ASIG = 4;
-	final static String []vNombres = {"Pepe" + "Juan"+ "Ana" + "Marta" + "Pedro" + "Maria"};
-	final static String []vNombresAsig = {"Lengua" + "Mates" + "Historia" + "Fisica"};
+	final static int MAX_ALUM = 6;
+	final static int MAX_ASIG = 4;
 
-	public static int pedirNota() {
-		Scanner entrada = new Scanner(System.in);
-		int num;
-		System.out.println("Introduce una nota:");
-		num = entrada.nextInt();
-		return num;
+	 public static void rellenarNotas(int[][] vNotas, String[] vAlumno, String[] vAsignaturas){
+	        Scanner teclado = new Scanner(System.in);
+	        int i, j;
+	        for(i = 0;i < MAX_ALUM;i++){
+	            System.out.println("NOTAS "+vAlumno[i]);
+	            System.out.println("-------------");
+	            for(j = 0;j < MAX_ASIG;j++){
+	                System.out.print(vAsignaturas[j]+": ");
+	                vNotas[i][j] = teclado.nextInt();
+	            }
+	        }
+	    }
+
+	public static void mostrarNotas(int aNotas[][],String[] vAlumnos,String[] vAsignaturas) {
+		int i, j;
+        for(i = 0;i < MAX_ALUM;i++){
+            System.out.println("\n\nNOTAS "+vAlumnos[i]);
+            System.out.println("------------");
+            for(j = 0;j < MAX_ASIG;j++){
+                System.out.print(vAsignaturas[j]+": "+aNotas[i][j]+" ");
+            }
+        }
 	}
 
-	public static void rellenarNotas(int aNotas[][]) {
-		Scanner entrada = new Scanner(System.in);
-		for(int i = 0;i < aNotas.length;i++){
-			for(int j = 0;j < aNotas[i].length;j++){    
-				int num = pedirNota();
-				aNotas[i][j]=num;
-			}
-		}
-	}
 
-	public static void mostrarNotas(int aNotas[][]) {
-		Scanner entrada = new Scanner(System.in);
-		for(int i = 0;i < aNotas.length;i++){
-			System.out.println();
-			for(int j = 0;j < aNotas[i].length;j++){
-				System.out.print("	["+ aNotas[i][j]+"]");
-			}
-		}
-	}
-	
-	public static float media(int aNotas[][]) {
-		int suma=0;
-		float media;
-		for(int i = 0; i < aNotas.length;i++) {
-			for(int j = 0; j< aNotas[i].length;j++) {
-				suma=suma+aNotas[i][j];
-			}
-		}
-		media= (suma/(aNotas[0].length*aNotas.length));
-		return media;
-	}
-	
-	public static void mejorAlumno(int aNotas [][], float media) {
+	public static void mejorAlumno(int aNotas [][], String[] vAlumnos) {
+		int suma, media=0, mejorAlumno=0;
 		for(int i = 0;i<aNotas.length;i++) {
+			suma=0;
 			for(int j = 0;i<aNotas[i].length;i++) {
-				
+				suma = suma + aNotas[i][j];
+			}
+			if(suma>media) {
+				media = suma;
+				mejorAlumno=i;
+			}
+
+		}
+		System.out.println("Mejor Alumno: " + vAlumnos[mejorAlumno]);
+	}
+
+	public static void peorAlumno(int aNotas[][], String[] vAlumnos) {
+		int suspensos=0, cont, peorAlumno = 0;
+		for(int i =0; i<aNotas.length;i++) {
+			cont=0;
+			for(int j=0;j<aNotas[i].length;j++) {
+				if(aNotas[i][j]<5) {
+					cont++;
+				}
+			}
+			if  (cont > suspensos){
+				suspensos = cont;
+				peorAlumno = i;
 			}
 		}
+		System.out.print("El alumno con mas suspensos es: " + vAlumnos[peorAlumno] + "y tiene " + suspensos + " suspensos");
 	}
-
-	public static void menu(int aNotas[][]) {
-		Scanner entrada = new Scanner(System.in);
-		int num;
-
-		do {
-			System.out.println("\n" + "Selecciona una opcion");
-			System.out.println("1- Relleanar las notas.");	
-			System.out.println("2- Mostrar las notas.");
-			System.out.println("3- Alumno con nota media mas alta.");
-			System.out.println("4- Alumno con mas suspensos.");
-			System.out.println("5- Asignatura mas dificil.");
-			System.out.println("6- Salir del programa.");
-			num = entrada.nextInt();
-
-			switch(num){
-			case 1:
-				System.out.println("Has seleccionado la opcion 1");
-				rellenarNotas(aNotas);
-				break;
-			case 2:
-				System.out.println("Has seleccionado la opcion 2");
-				mostrarNotas(aNotas);
-				break;
-			case 3:
-				System.out.println("Has seleccionado la opcion 3");
-
-				break;
-			case 4:
-				System.out.println("Has seleccionado la opcion 4");
-
-				break;
-			case 5:
-				System.out.println("Salir del programa");
-				break;
-			default:
-				System.out.println("Opción no válida");
-			}
-		}while(num != 6);
-
-		entrada.close();
-
+	
+	public static void asigDificil(int aNotas[][], String[] vAsignaturas) {
+		int i, j;
+        int suma, menorMedia = Integer.MAX_VALUE;
+        int asigDificil = 0;
+        for(j = 0;j < MAX_ASIG;j++){
+            suma = 0;
+            for(i = 0;i < MAX_ALUM;i++){
+                suma = suma + aNotas[i][j];
+            }
+            if (suma < menorMedia){
+                menorMedia = suma;
+                asigDificil = j;
+            }
+        }
+        System.out.println("Asignatura más difícil: "+ vAsignaturas[asigDificil]);
 	}
 
 
-	public static void main(String[] args) {
-		int [][]aNotas = new int [ASIG][ALUM];
-
-		menu(aNotas);
-
-	}
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int[][] vNotas = new int[MAX_ALUM][MAX_ASIG];
+        String[] vAlumnos = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
+        String[] vAsignaturas = {"Lengua", "Matemáticas", "Historia", "Físca"};
+        int opc;
+        do{
+            System.out.println("\nMENÚ CLASE");
+            System.out.println("1. Rellenar Notas");
+            System.out.println("2. Mostrar Notas");
+            System.out.println("3. Mejor alumno");
+            System.out.println("4. Peor alumno");
+            System.out.println("5. Asignatura más difícil");
+            System.out.println("6. Salir");
+            System.out.print("   Opción: ");
+            opc = teclado.nextInt();
+            switch(opc){
+                case 1:
+                    rellenarNotas(vNotas, vAlumnos, vAsignaturas);
+                    break;
+                case 2:
+                    mostrarNotas(vNotas, vAlumnos, vAsignaturas);
+                    break;
+                case 3:
+                    mejorAlumno(vNotas, vAlumnos);
+                    break;
+                case 4:
+                    peorAlumno(vNotas, vAlumnos);
+                    break;
+                case 5:
+                    asigDificil(vNotas, vAsignaturas);
+                    break;
+                case 6:
+                    System.out.println("¡Hasta la próxima!");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+        }while(opc != 6);
+    }
 
 }
